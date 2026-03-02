a6336bd5a5bcd3c42875e212228048370228b18d
fridapaulina <fridapaulina.arvidsson@gmail.com>
2026-03-02 15:23:42 +0100

    Lagt till ny fil

---
+++ ex_variables.py
@@ -0,0 +1,46 @@
+#1
+
+name = "Frida"
+age = 39
+
+print("Hej jag heter", name, "och är", age, "år gammal")
+
+#2
+
+number_string = "1500"
+
+print (int(number_string) + 500)
+
+#or
+number_int = int(number_string) + 500
+
+print (number_int)
+
+#3
+
+pi = 3.14159
+
+print(round(pi,2))
+
+#or
+pi_rounded = round(pi,2)
+
+print(pi_rounded)
+
+#4
+price = 28
+quantity = 3
+
+print(f"Total price {price*quantity}")
+
+#5
+
+x = "10"
+
+print(type(x))
+
+#6
+is_active = True
+
+
+
